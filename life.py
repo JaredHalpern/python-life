a7a1ad6ad890ea26e8759e4de69107f16c2f637f
Jared Halpern <jareeedo@gmail.com>
2017-01-28 19:44:09 -0500

    added gliders

--- life.py
+++ life.py
@@ -7,31 +7,41 @@ ON = 255
 OFF = 0
 vals = [ON, OFF]
 
-def update(frameNum, img, grid, N):
-	# copy grid since we require 8 neighbors for calculation
-	# and we go line by line 
-	newGrid = grid.copy()
-	for i in range(N):
-		for j in range(N):
-			# compute 8-neighbor sum
-			# using toroidal boundary conditions - x and y wrap around 
-			# so that the simulaton takes place on a toroidal surface.
-			total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
-					grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
-					grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
-					grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
-			# apply Conway's rules
-			if grid[i, j]  == ON:
-				if (total < 2) or (total > 3):
-					newGrid[i, j] = OFF
-			else:
-				if total == 3:
-					newGrid[i, j] = ON
-	# update data
-	img.set_data(newGrid)
-	grid[:] = newGrid[:]
-	return img,
+def addGlider(i, j, grid):
+    """adds a glider with top left cell at (i, j)"""
+    glider = np.array([[0,    0, 255], 
+                       [255,  0, 255], 
+                       [0,  255, 255]])
+    grid[i:i+3, j:j+3] = glider
+# 
+def addGosperGliderGun(i, j, grid):
+    """adds a Gosper Glider Gun with top left cell at (i, j)"""
+    gun = np.zeros(11*38).reshape(11, 38)
 
+    gun[5][1] = gun[5][2] = 255
+    gun[6][1] = gun[6][2] = 255
+
+    gun[3][13] = gun[3][14] = 255
+    gun[4][12] = gun[4][16] = 255
+    gun[5][11] = gun[5][17] = 255
+    gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
+    gun[7][11] = gun[7][17] = 255
+    gun[8][12] = gun[8][16] = 255
+    gun[9][13] = gun[9][14] = 255
+
+    gun[1][25] = 255
+    gun[2][23] = gun[2][25] = 255
+    gun[3][21] = gun[3][22] = 255
+    gun[4][21] = gun[4][22] = 255
+    gun[5][21] = gun[5][22] = 255
+    gun[6][23] = gun[6][25] = 255
+    gun[7][25] = 255
+
+    gun[3][35] = gun[3][36] = 255
+    gun[4][35] = gun[4][36] = 255
+
+    grid[i:i+11, j:j+38] = gun
+# 
 def randomGrid(N):
 	return np.random.choice(vals, N * N, p=[0.1, 0.9]).reshape(N, N)
 
@@ -39,7 +49,7 @@ def main():
 	parser = argparse.ArgumentParser(description="Game of Life")
 	parser.add_argument('--grid-size', dest='N', required=False)
 	parser.add_argument('--interval', dest='interval', required=False)
-	parser.add_argument('--mov-file', dest='mov-file', required=False)
+	parser.add_argument('--mov-filename', dest='mov_filename', required=False)
 	parser.add_argument('--glider', action='store_true', required=False)
 	parser.add_argument('--gosper', action='store_true', required=False)
 	args = parser.parse_args()
@@ -70,10 +80,35 @@ def main():
 		interval = updateInterval,
 		save_count = 50)
 
-	if args.mov-file:
-		ani.save(args.mov-file, fps=30, extra_args=['-vcodec', 'libx264'])
+	if args.mov_filename:
+		ani.save(args.mov_filename, fps=30, extra_args=['-vcodec', 'libx264'])
 
 	plt.show()
+
+def update(frameNum, img, grid, N):
+	# copy grid since we require 8 neighbors for calculation
+	# and we go line by line
+	newGrid = grid.copy()
+	for i in range(N):
+		for j in range(N):
+			# compute 8-neighbor sum
+			# using toroidal boundary conditions - x and y wrap around 
+			# so that the simulaton takes place on a toroidal surface.
+			total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+					grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+					grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+					grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+			# apply Conway's rules
+			if grid[i, j]  == ON:
+				if (total < 2) or (total > 3):
+					newGrid[i, j] = OFF
+			else:
+				if total == 3:
+					newGrid[i, j] = ON
+	# update data
+	img.set_data(newGrid)
+	grid[:] = newGrid[:]
+	return img
 	
 if __name__ == '__main__':
 	main()
